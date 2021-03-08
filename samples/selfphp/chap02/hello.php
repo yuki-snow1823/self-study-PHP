f36919df141a78e90b05042c3cfdeb081e978eb9
yuki-snow1823 <magic.iroha@gmail.com>
2021-03-09 07:10:25 +0900

    init

---
+++ samples/selfphp/chap02/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>PHPの基本</title>
+</head>
+<body>
+<?php
+ // print命令は指定された文字列を表示するための命令です。
+print 'こんにちは、世界！<br />';
+print 'こんにちは、皆さん！';
+?>
+</body>
+</html>
