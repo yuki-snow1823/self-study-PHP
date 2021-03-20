4962ad6b33b8d446c3f9d0661564056bf827df60
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-03-20 09:47:00 +0900

    演算子と練習問題

---
+++ section3/3-2_practice.php
@@ -0,0 +1,12 @@
+<?php
+
+$x = "hoge";
+
+$x == 1 ? $flag = 1 : $flag = 2;
+
+// print $flag;
+
+var_dump('1.44E2' == 144);
+var_dump('1.44E2' === 144);
+var_dump('0x10' == '16');
+var_dump('H' == 0); // true...
