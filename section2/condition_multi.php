97c43fa415b406de6e896e713a77f47219bdeb28
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-03-20 09:26:18 +0900

    処理じゅん

--- section2/condition_multi.php
+++ section2/condition_multi.php
@@ -1,4 +1,4 @@
 <?php 
-print true ? 1 : false ? 'OK' : 'NG';
-//print (true ? 1 : false) ? 'OK' : 'NG';
-//print true ? 1 : (false ? 'OK' : 'NG');
+print false ? 1 : false ? 'OK' : 'NG'; // 括弧がないと先頭だけ処理して終わり
+print (true ? 1 : false) ? 'OK' : 'NG';
+print false ? 1 : (true ? 'OK' : 'NG');
