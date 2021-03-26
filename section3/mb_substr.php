c1794be2d887b1abeb4fb5373a38fbfe8386233c
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-03-27 08:19:50 +0900

    mb_substrの仕様

--- section3/mb_substr.php
+++ section3/mb_substr.php
@@ -2,9 +2,13 @@
 mb_internal_encoding('UTF-8');
 $str = 'WINGSプロジェクト';
 print mb_substr($str, 5, 2);
+// 0から数えるから6文字目から２つ
 print '<br />';
 print mb_substr($str, 5);
+// 6文字目以降
 print '<br />';
 print mb_substr($str, 5, -4);
+// 6文字目以降、末尾4文字カット
 print '<br />';
 print mb_substr($str, -6, 2);
+// 後方6文字から２つ
