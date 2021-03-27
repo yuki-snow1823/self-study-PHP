e355278acdca49153dcf2419a4330ca9af85344a
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-03-27 09:01:16 +0900

    move

--- section5/mb_send_mail.php
+++ section5/mb_send_mail.php
@@ -5,6 +5,8 @@ $body = "こんにちは、mb_send_mail関数！\nどうですか？";
 $headers  = "From : user01@example.com\n";
 $headers .= "Cc : yamada@example.com\n";
 $headers .= "X-Mailer : PHP 7\n";
+
+// 宛先　件名　ボディ　ヘッダー
 if (mb_send_mail($to, $subject, $body, $headers)) {
   print 'メール送信に成功しました。';
 } else {
