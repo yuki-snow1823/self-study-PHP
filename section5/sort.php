59e9e3dd4d1dbd17e0d3d346f11670f9c74f4d6f
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-03-27 10:00:08 +0900

    sort

--- section5/sort.php
+++ section5/sort.php
@@ -4,6 +4,7 @@ $data = ['Tennis', 'Swimming', 'Soccer', 'Baseball'];
 sort($data, SORT_STRING);
 print_r($data);
 rsort($data, SORT_STRING);
+// 小さい順
 print_r($data);
 $data2 = ['Tennis' => 1, 'Swimming' => 1, 'Soccer' => 11 , 'Baseball' => 9];
 sort($data2, SORT_NUMERIC);
@@ -13,3 +14,5 @@ asort($data3, SORT_NUMERIC);
 print_r($data3);
 ksort($data3, SORT_STRING);
 print_r($data3);
+
+// ksortはkeysortのこと
