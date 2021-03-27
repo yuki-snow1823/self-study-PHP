5be67e7f6cd5f8f8696c535c8c80c57d0ccb3460
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-03-27 09:37:33 +0900

    要復習

--- section5/array_slice.php
+++ section5/array_slice.php
@@ -4,4 +4,5 @@ $data = ['高江', '青木', '片渕', '和田', '花田', '佐藤'];
 print_r(array_slice($data, 2, 3));
 print_r(array_slice($data, 2, 3, true));
 print_r(array_slice($data, 4));
+// むずい
 print_r(array_slice($data, -4, -3));
--- section5/array_splice.php
+++ section5/array_splice.php
@@ -2,7 +2,9 @@
 <?php
 $data = ['高江', '青木', '片渕', '和田', '花田', '佐藤'];
 print_r(array_splice($data, 2, 3, ['日尾', '掛谷', '薄井']));
+// 取った要素が出力され、配列には入ったものも含まれて表示される
 print_r($data);
+// 負数の場合、一番後ろがマイナス1
 print_r(array_splice($data, -3, -2, ['長田', '杉山']));
 print_r($data);
 print_r(array_splice($data, 3));
