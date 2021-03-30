cb20c35618cb29c321d722697d10b3bf02146db6
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-03-31 05:32:49 +0900

    dd higher function

--- section6/higher_order_sum.php
+++ section6/higher_order_sum.php
@@ -5,12 +5,12 @@ function my_array_walk(array $array, callable $func) {
   }
 }
 
-$result = 0;
+$result = 0; // 内部的に自由にコールバック作れる
 function sum(float $value, int $key) {
   global $result;
   $result += $value;
 }
 
 $data = [100, 50, 10, 5];
-my_array_walk($data, 'sum');
+my_array_walk($data, 'sum'); //呼び出し元で好きなものを呼べる
 print "合計値：{$result}";
