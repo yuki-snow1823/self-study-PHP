c14ed2296c41ee3060ee9c7c6392be682ac04dc0
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-04-07 06:54:54 +0900

    add: ディレクトリの検索

--- section7/p_dir.php
+++ section7/p_dir.php
@@ -1,5 +1,8 @@
 <?php
 $dir = new DirectoryIterator('./');
+
+// ファイルじゃなくてディレクトリを見てくる
+
 foreach ($dir as $file) {
   if ($file->isFile()) {
     print mb_convert_encoding($file->getFileName(), 'UTF-8', 'SJIS-WIN'). '<br />';
