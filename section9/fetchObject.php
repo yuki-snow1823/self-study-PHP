a619e80b9ca1e2812bb1593fd6b1ce67661e7204
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-04-21 07:30:41 +0900

    fetxh結果をオブジェクトに書き込み

--- section9/fetchObject.php
+++ section9/fetchObject.php
@@ -4,6 +4,6 @@ require_once 'Book.php';
 
 $db = getDb();
 $stt = $db->query("SELECT * FROM book WHERE isbn='978-4-7981-4102-2'");
-if ($row = $stt->fetchObject('Book', [0.1])) {
+if ($row = $stt->fetchObject('Book', [0.1])) { //bookクラスに渡すことができる
   print "{$row->title}：{$row->discount()}円";
 }
